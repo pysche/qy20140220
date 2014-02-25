4a0cc7d9a78a6f740dd96b879fd44ff5347cc954
pang <pysche@ipbfans.org>
2014-02-25 22:19:36 +0800

    updated

---
+++ application/modules/default/controllers/ProjectsController.php
@@ -0,0 +1,21 @@
+<?php
+
+class ProjectsController extends Bc_Controller_Action_Weshop {
+	
+	public function init() {
+		parent::init();
+
+		$this->nLogin();
+		$this->view->searchKeys = $this->searchKeys = array(
+			'Code' => '项目编码',
+			'Name' => '项目名称',
+			);
+
+		$this->view->MName = $this->MName = '备案项目管理';
+	}
+
+	public function indexAction() {
+		
+	}
+	
+}
