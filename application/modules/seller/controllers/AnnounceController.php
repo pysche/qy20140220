d10f74606dd96c2fb36113b8871c00bb807d51ee
pang <pysche@ipbfans.org>
2014-03-06 23:16:23 +0800

    seller init

--- application/modules/seller/controllers/AnnounceController.php
+++ application/modules/seller/controllers/AnnounceController.php
@@ -2,7 +2,17 @@
 
 class Seller_AnnounceController extends Bc_Controller_Action_Seller {
 
-	public function indexAction() {
-		
+	public function init() {
+		$this->force_where = 'Status=1';
+		$this->mName = 'Announce';
+
+		parent::init();
+		$this->nLogin();
+
+		$this->view->searchKeys = $this->searchKeys = array(
+			'Title' => '公告标题',
+			);
+
+		$this->view->MName = $this->MName = '公告浏览';
 	}
 }
---
+++ application/modules/seller/controllers/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+class Seller_WelcomeController extends Bc_Controller_Action_Seller {
+	protected $restrictRole = '';
+	
+	public function init() {
+		parent::init();
+
+		$this->nLogin();
+
+		$this->force_where = 'Status=1';
+		$this->mName = 'Announce';
+	}
+
+	public function indexAction() {
+		$this->view->LastLogin = $this->sess->get('last_login');
+
+		parent::indexAction();
+	}
+}
